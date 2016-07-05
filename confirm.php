4baa6457dadd70cbe199141f8a3fc7916b93c6aa
利剑斩无明 <lijianzhanwuming@163.com>
2016-07-05 17:02:46 +0800

    添加载入动画,微信支付

--- confirm.php
+++ confirm.php
@@ -6,6 +6,10 @@
  * Time: 上午9:41
  */
 
+require_once "./pay/wxpay/WxPay.JsApiPay.php";
+
+date_default_timezone_set("Asia/Shanghai");
+
 $payChannel = 'WXPAY';//手机版默认微信支付!
 
 $fhId = $_POST['fh_id'];
@@ -100,19 +104,54 @@ if ($members != "") {
             <p style="color:grey;">返回重填</p>
         </button>
     </div>
+
+
+    <!-- 创建订单载入画面 -->
+    <div class="ui-loading-block" id="now_loading">
+        <div class="ui-loading-cnt">
+            <i class="ui-loading-bright"></i>
+            <p id="loading_text">创建订单...</p>
+        </div>
+    </div>
+    <script type="text/javascript" class="demo-script"></script>
+
+    <!-- 订单创建失败提示 -->
+    <div class="ui-dialog">
+        <div class="ui-dialog-cnt">
+            <header class="ui-dialog-hd ui-border-b">
+                <h3>错误!</h3>
+                <i class="ui-dialog-close" data-role="button" onclick="closeDialog()"></i>
+            </header>
+            <div class="ui-dialog-bd">
+                <h4>订单创建失败!</h4>
+            </div>
+            <div class="ui-dialog-ft">
+                <button type="button" data-role="button" onclick="closeDialog()">取消</button>
+                <button type="button" data-role="button" onclick="retryCreateOrder()">重试</button>
+            </div>
+        </div>
+    </div>
+    <script class="demo-script"></script>
+
 </section>
+
 <script src="./lib/zepto.min.js"></script>
 <script>
+
+    var randNum =  Math.ceil(Math.random()*89)+10;
+    order_no = (new Date()).valueOf() + "" + randNum;
 
     function refill(){
         window.location.href="signup.php";
     }
 
     function createOrder(){
-        alert("createOrder");
+        $("#now_loading").addClass("show");
+        $("#loading_text").text("创建订单...");
         $.ajax({
             url: "CreateOrder.php",
             data: {
+                order_no : order_no,
                 type : '<?php echo $sxItem=="any"?"随喜不限":$sxItem; ?>',
                 money : '<?php echo $money; ?>',
                 pay_channel : '<?php echo $payChannel; ?>',
@@ -126,13 +165,57 @@ if ($members != "") {
             cache: false,
             dataType: 'json',
             success: function (data) {
-                alert(JSON.stringify(data));
+                $("#now_loading").removeClass("show");
+                if( data.error_code ){
+                    $(".ui-dialog").addClass("show");
+                }else{
+                    callpay();
+                }
             },
             error: function () {
                 alert("创建订单请求失败!");
             }
         });
     }
+
+    function retryCreateOrder(){
+        $(".ui-dialog").removeClass("show");
+        createOrder();
+    }
+
+    function closeDialog(){
+        $(".ui-dialog").removeClass("show");
+    }
+
+    //调用微信JS api 支付
+    function jsApiCall(){
+        WeixinJSBridge.invoke(
+            'getBrandWCPayRequest',
+            <?php echo $jsApiParameters; ?>,
+            function(res){
+                if( !res.err_code ){
+                    queryOrder();
+                }else{
+                    alert(res.err_code+res.err_desc+res.err_msg);
+                }
+            }
+        );
+    }
+
+    function callpay()
+    {
+        if (typeof WeixinJSBridge == "undefined"){
+            if( document.addEventListener ){
+                document.addEventListener('WeixinJSBridgeReady', jsApiCall, false);
+            }else if (document.attachEvent){
+                document.attachEvent('WeixinJSBridgeReady', jsApiCall);
+                document.attachEvent('onWeixinJSBridgeReady', jsApiCall);
+            }
+        }else{
+            jsApiCall();
+        }
+    }
+
 
 </script>
 </body>
