f4752090892b1c5739f712cfd0dbd76128347b71
利剑斩无明 <lijianzhanwuming@163.com>
2016-07-05 21:23:20 +0800

    添加法会id,电话

--- 数据库设计.sql
+++ 数据库设计.sql
@@ -13,6 +13,7 @@ CREATE TABLE `fh_fahui` (
 
 CREATE TABLE `fh_order` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `fh_id` int(10) unsigned NOT NULL COMMENT '法会id',
   `order_no` varchar(30) NOT NULL UNIQUE COMMENT '订单号',
   `type` varchar(20) NOT NULL COMMENT '随喜类型',
   `pay_status` enum('NONE','PAYERROR','PAYSUCCESS','FINISH','DRAW') DEFAULT NULL COMMENT '支付状态',
@@ -20,6 +21,7 @@ CREATE TABLE `fh_order` (
   `money` int NOT NULL COMMENT '随喜金额',
   `host_name` varchar(20) NOT NULL COMMENT '斋主姓名',
   `host_birthday` varchar(50) NOT NULL COMMENT '斋主农历生日',
+  `phone` varchar(20) NOT NULL COMMENT '手机号码',
   `member` varchar(500) DEFAULT NULL COMMENT '其他成员',
   `address` varchar(500) NOT NULL COMMENT '居住地址',
   `huixiang` varchar(500) NOT NULL COMMENT '回向内容',
